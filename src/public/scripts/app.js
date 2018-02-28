c10783b74f289b45ae03c1c6dcd86ed9965c0f50
ravi <astroravi97@gmail.com>
2018-02-28 19:51:00 +0530

    login

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -1,4 +1,6 @@
 	var createAccountButtonEl= document.getElementById('createAcoountBtn');
+	var loginButtonEl= document.getElementById('loginBtn');
+	//on Click create Account Button: Client side 
 	createAccountButtonEl.onclick=function () {
 		var usernameEl= document.getElementById('userName');
 		var NameOfUser= usernameEl.value;
@@ -17,10 +19,10 @@
 			}
 			console.dir(objectToBeSent);
 			createAccount(objectToBeSent);
+			console.log("crossed");
 		};
-
 			
-	}
+	};
 
 function createAccount (payload) {
 	fetch('/createAccount', {
@@ -30,4 +32,36 @@ function createAccount (payload) {
 		},
 		 body: JSON.stringify(payload)});
 
+}
+
+
+var loginbtn = document.getElementById('loginBtn');
+loginbtn.onclick = function() {
+	var email= document.getElementById('emails').value;
+	var passWord = document.getElementById('password1').value;
+	var objCollection = {
+		email: email,
+		password: passWord
+	}
+	verifyLogin(objCollection);
+};
+
+function verifyLogin (payload) {
+	fetch('/login', {
+		method: 'post',
+		headers: {
+			'Content-Type':'application/json; charset=utf-8'
+		},
+		 body: JSON.stringify(payload)}).then( function (res) {
+		 	return 	res.json()
+		 }).then(function (resp) {
+		 	console.log('final', resp)
+		 	if(resp.success === 'SUCCESS') {
+		 		alert('Logged in !!');
+		 	} else  {
+		 		alert('Not able to login !!');
+		 	}
+		 }).catch( function  (e) {
+		 	console.log('Error', e);
+		 });
 }
--- src/server.js
+++ src/server.js
@@ -19,17 +19,43 @@ var con = mysql.createConnection({
 });
 
 app.post('/createAccount', function (req, res) {
-
 	con.connect(function(err) {
+		console.log("inside");
 		if (err) throw err;
-		console.log("Connected!");
-		var sql = "INSERT INTO user (username, email, pwd) VALUES ("+req.body.user+","+ req.body.email+","+ req.body.password +")";
+			console.log("Connected!");
+			var sql = "INSERT INTO user (username, email, pwd) VALUES ('"+req.body.user+"','"+ req.body.email+"','"+ req.body.password +"');";
+			console.log('swl', sql);
 		con.query(sql, function (err, result) {
-			if (err) throw err;
+			if (err) {
+				console.log('err',err);
+				throw err
+			};
 				console.log("1 record inserted");
 			});
 		});
-		console.log(req.body);
-	})
+	});
+app.post('/login', function (req, res){
+	con.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
+	var sql= "SELECT * FROM user WHERE email='"+ req.body.email+ "'AND pwd='" +req.body.password+	"';";
+	console.log("value sql=", sql);
+	con.query(sql, function (err, result) {
+			if (err) {
+				console.log('err', error);
+				throw err;
+			}
+			console.log("fetched from database");
+			console.log('res',result);
+			if( result.length > 0) {
+				res.send({code: 200, success: 'SUCCESS'});
+			} else {
+				res.send({code: 200, success: 'FAILURE'});
+
+			}
+			});
+		}
+		)});
+	/*console.log(req.body);*/
 
 app.listen(3000, function () {console.log('Example app listening on port 3000!')});
